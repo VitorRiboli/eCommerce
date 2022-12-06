2581bd9d041a5db792a7168a510de360aabe7730
vitorriboli <vitorriboli1@gmail.com>
2022-12-06 06:33:41 -0300

    Feat: BCrypt for password

--- src/main/java/com/commerce/vitor/VitorApplication.java
+++ src/main/java/com/commerce/vitor/VitorApplication.java
@@ -3,11 +3,13 @@ package com.commerce.vitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class VitorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VitorApplication.class, args);
 	}
+
 
 }
--- src/main/java/com/commerce/vitor/config/WebSecurityConfig.java
+++ src/main/java/com/commerce/vitor/config/WebSecurityConfig.java
@@ -3,12 +3,18 @@ package com.commerce.vitor.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
 
     //Classe de configuração
+    //@Bean defini como componente reaproveitavel do sistema
+    @Bean //Método encriptação de senha
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean //@Bean define componente do sistema
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
