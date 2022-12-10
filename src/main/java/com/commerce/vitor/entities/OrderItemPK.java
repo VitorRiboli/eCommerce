275b9ab1ce03f4478485ad8dbf479a0adddd576f
vitorriboli <vitorriboli1@gmail.com>
2022-12-10 07:53:57 -0300

    Dev profile and Postgres

--- src/main/java/com/commerce/vitor/entities/OrderItemPK.java
+++ src/main/java/com/commerce/vitor/entities/OrderItemPK.java
@@ -4,6 +4,7 @@ import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class OrderItemPK implements Serializable {
@@ -33,5 +34,17 @@ public class OrderItemPK implements Serializable {
 
     public void setProduct(Product product) {
         this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItemPK that)) return false;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
     }
 }
